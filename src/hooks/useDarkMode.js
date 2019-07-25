337753e3acfa8991725223f4ca69ade833d16ab1
Jordin Kolman <git@github.com>
2019-07-25 15:26:13 -0500

    Finished MVP dark mode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+import { useLocalStorage } from './useLocalStorage'
+
+export const useDarkMode = () => {
+
+const [view, setView] = useLocalStorage('view', { darkMode: false })
+
+
+    useEffect(() => {
+        if (view) {
+            {document.querySelector('body').classList.add('dark-mode')}
+        } else {
+            {document.querySelector('body').classList.remove('dark-mode')}
+        }
+    }, [view])
+
+
+return [view, setView];
+
+}
